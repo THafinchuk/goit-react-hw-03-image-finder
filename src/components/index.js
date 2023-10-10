d6a75f7d851dce4d3fcdbe68b4e877baa704b4f5
THafinchuk <t.hafinchuk@gmail.com>
2023-10-10 21:59:52 +0300

    search

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Button } from './Button/Button';
 export { default as ImageGallery } from './ImageGallery/ImageGallery';
 export { default as Modal } from './Modal/Modal';
-export { default as Searchbar } from './SearchBar/SearchBar';
+export { default as Searchbar } from './Searchbar/SearchBar';
 export { default as Loader } from './Loader/Loader';
